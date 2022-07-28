4ba13ad54b20d6c00aa1e21f6f9cff15145d0994
Santiago <srueda9331@gmail.com>
2022-07-28 12:47:49 -0500

    Changes to render countries

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Switch }  from 'react-router-dom'
+import Landing from './components/LandingPage';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Henry Countries</h1>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/home' component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
