266db116f37a101a2c05b68626bbed0bdf463d14
Santiago <srueda9331@gmail.com>
2022-07-31 21:28:12 -0500

    detail component only missing styles

--- client/src/components/CountryDetail.jsx
+++ client/src/components/CountryDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCountryDetail } from "../actions/actions";
+import { cleanDetail, getCountryDetail } from "../actions/actions";
 import { Link } from "react-router-dom";
 
 export default function CountryDetail(props){
@@ -9,6 +9,7 @@ export default function CountryDetail(props){
 
   useEffect(() => {
     dispatch(getCountryDetail(props.match.params.id));
+    return ()=> dispatch(cleanDetail())
   }, [dispatch])
 
   const countryDetail = useSelector(state => state.detail)
@@ -24,10 +25,20 @@ export default function CountryDetail(props){
           <img src={countryDetail.image} alt={countryDetail.name} />
           <h2>Continent: {countryDetail.continent}</h2>
           <h3>Capital: {countryDetail.capital}</h3>
-          <h3>Subregion: {countryDetail.subregion}</h3>
-          <h3>Area: {countryDetail.area} km2</h3>
+          {
+          countryDetail.subregion?
+          <h3>Subregion: {countryDetail.subregion}</h3> : null
+          }
+          <h3>Area: {countryDetail.area} km²</h3>
           <h3>Population: {countryDetail.population} citizens</h3>
+       
+          {
+          countryDetail.activities? 
+          countryDetail.activities.map(el => <p><b>Activity {el.id}</b>: <b>name:</b> {el.name}, <b>difficulty:</b> {el.difficulty}, <b>length:</b> {el.length}, <b>season:</b> {el.season} </p>  ) : null
+          }
+          
         </div> ) 
+      
         :
         (<div>
           <p>Sorry, the country you're looking for doesn't exist <img src='https://cdn.icon-icons.com/icons2/1123/PNG/512/sadface_79592.png' alt='sad face' width='30px' height='30px'></img></p>
@@ -35,7 +46,9 @@ export default function CountryDetail(props){
 }
       
       <Link to='/home'>
-        <button>Get Back</button>
+        <div>
+          <button>Get Back</button>
+        </div>
       </Link>
     </div>
   )
