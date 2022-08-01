7f0e4e85004231ab6c5a1a70b002ef9522c2ac78
Santiago <srueda9331@gmail.com>
2022-07-31 21:23:17 -0500

    relationship between tables

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
-const { Activities } = require('../db')
+const { Activities, Country } = require('../db')
+const axios = require('axios');
+
 
 
 const router = Router();
@@ -14,31 +16,69 @@ function* newId(){
 
 let getNewId = newId();
 
+const getAllCodes = async () => {
+  let getAll = await axios.get('https://restcountries.com/v3/all')
+  let getCodes = getAll.data.map(c => c.cca3)
+  return getCodes
+}
+
+router.get('/', async (req, res) => {
+  try {
+    let codes = await getAllCodes()
+ 
+    codes.forEach(el => {
+      Activities.findOrCreate({
+        where: { name: el}
+      })
+    });
+    let findCodes = await Activities.findAll({where : addCountry});
+    res.status(201).send(findCodes)
+    
+  } catch (error) {
+    console.log(error);
+  }
+})
+  //   return (
+    // Activities.findOrCreate({
+    //   where: { countriesAdd: c}
+    // }) 
+  // )
+
+    // res.send(c)
+
+
+
 router.post('/', async (req, res) => {
-  const { name, difficulty, length, season } = req.body;
+  const { name, difficulty, length, season, countries } = req.body;
   
   try {
-    if(!name || !difficulty || !length || !season) res.status(404).json({msg : 'Some values are missing'})
-   
+    if(!name || !difficulty || !length || !season || !countries ) res.status(404).json({msg : 'Some values are missing'})
+    
     
     let create = await Activities.create({
       id: getNewId.next().value,
       name,
       difficulty,
       length, 
-      season
+      season,
+      countries: []
     })
-    
-    res.status(201).send(create)
+    if(difficulty !== 1 && difficulty !== 2 && difficulty !== 3 && difficulty !== 4 && difficulty !== 5) res.status(404).json({msg :'Write only a number between 1 and 5'})
+    if(season !== 'Summer' && season !== 'Autumn'  && season !== 'Autumn' && season !== 'Spring') res.status(404).json({msg :'Write only one of four seasons (Summer, Winter, Autumn, Spring)'})
+    let countryDb = await Country.findAll({
+      where: {name : countries}
+    })
+    create.addCountries(countryDb)
+    // console.log(Activities.__proto__);
+    console.log('other', create.__proto__);
+
+    res.status(200).send('Activity created')
+   
   } catch (error) {
     console.log(error);
   }
 })
 
-router.get('',  (req, res) => {
-  let findAll = Activities.findAll()
-  .then(resp => res.send(resp))
-})
 
 
 // { 
@@ -46,6 +86,7 @@ router.get('',  (req, res) => {
 //   "difficulty" : "hard", 
 //   "length" : "4 days", 
 //   "season": "Winter "
+//    "countries" : ["Colombia", "Argentina"]
 // }
 
 module.exports = router;
