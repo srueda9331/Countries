265c1f82757c2c96fd9852f43e994f1059efc6d2
Santiago <srueda9331@gmail.com>
2022-07-31 21:29:14 -0500

    rendering activities for countries

--- client/src/components/Countries.jsx
+++ client/src/components/Countries.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default function Country({name, image, continent}){
+export default function Country({name, image, continent, activities }){
 
   return (
     <div>
       <h2>{name}</h2>
       <img src={image} alt={name} width='180px' height='135px'/>
       <h3>{continent}</h3>
+      
+      {
+        activities.length > 0? activities.map(el => <span>{el.name + ', '}</span>) : null
+      }
+  
     </div>
   )
 }
