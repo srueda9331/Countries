16e67d29b413d0cf6a248201e377b005b8917cc4
Santiago José Rueda Valencia <90795981+srueda9331@users.noreply.github.com>
2022-09-25 17:56:42 -0500

    using async await instead of classic promises while getting countries

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -6,70 +6,60 @@ const axios = require('axios')
 const router = Router();
 
 
-router.get('/', (req, res) => {
- 
+router.get('/', async (req, res) => {
+  // If no name, then find all ---> Si no hay nombre, entonces encontrar todos
+  const name = req.query.name    
   try {
-    const name = req.query.name
-
     if(name){
-      Country.findAll({
+      let findName = await Country.findAll({
         include : Activities,
         where: {
           name : {
-            [Op.iLike] : '%' + name + '%'
+            [Op.iLike] : '%' + name.toLowerCase() + '%'
           }
         }
       })
-      .then(response => {
-        let found = response.filter(p => p.name)
-        found.length > 0? res.send(found) : res.status(404).send('Country not found')
-      })  
-    } 
-    
-    Country.findAll({
+
+      findName.length? res.status(201).send(findName) : res.status(404).json({msg: 'Could not find that country name'})
+    }
+    // Find all countries already created at database --> Encontrar todos los paises en base de datos
+    let findEveryCountry = await Country.findAll({
       include: {
         model : Activities,
-        attributes : ['id', 'name', 'difficulty', 'length', 'season'],
+        attributes : ['id', 'name', 'difficulty', 'duration', 'season'],
         through: {
           attributes: []
         }
       }
-    }).then(response => {
-      if(response.length > 0){
-        res.send(response)
-      } 
-
-      else{
-
-        axios.get('https://restcountries.com/v3/all')
-       .then((response) => {
-         let countries = response.data.map(country => {
-           return {
-             id: country.cca3,
-             name: country.name.common,
-             image: country.flags[1],
-             continent: country.continents[0],
-             capital: country.capital? country.capital[0] : 'No capital',
-             subregion: country.subregion,
-             area: country.area,
-             population: country.population
-             } 
-         })  
-        
-        Country.bulkCreate(countries).then(result => res.send(result))
-        .catch(error => res.status(404).json({msg : 'Something went wrong'}))
-        
-       })
-
-      }
     })
+    // If they're not created yet, go to else and Bulkcreate first --> Si no existen en bd entonces crearlos
+    if(findEveryCountry.length > 0) res.status(201).send(findEveryCountry) 
+    
+    else {
+    // for default it will create the countries at db, if all conditions before weren't true
+    let getAllCountries = await axios.get('https://restcountries.com/v3/all')
+    let countries = getAllCountries.data.map(country => {
+      return {
+        id: country.cca3,
+        name: country.name.common,
+        image: country.flags[1],
+        continent: country.continents[0],
+        capital: country.capital? country.capital[0] : 'No capital',
+        subregion: country.subregion,
+        area: country.area,
+        population: country.population
+        } 
+    })  
+   
+   
+   let createEverything = await Country.bulkCreate(countries)
+   createEverything.length > 0? res.send(createEverything) : res.status(404).send('Country not found')
+  }
     
   } catch (error) {
     console.log(error);
   }
 })
-
-
 
 
 router.get('/:id', async (req, res) => {
@@ -79,7 +69,7 @@ router.get('/:id', async (req, res) => {
     country = await Country.findByPk(id, {
       include: {
         model : Activities,
-        attributes : ['id', 'name', 'difficulty', 'length', 'season'],
+        attributes : ['id', 'name', 'difficulty', 'duration', 'season'],
         through: {
           attributes: []
         }
@@ -93,7 +83,6 @@ router.get('/:id', async (req, res) => {
     console.log(error);
   }
 })
-
 
 
 
